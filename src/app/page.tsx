9e1710129800e5fc1400095023314e0557339421
Rodrigo <rodricelis@hotmail.com>
2025-05-24 18:28:31 -0400

    adding material ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Accordion, AccordionActions, AccordionDetails, AccordionSummary, Button, Typography } from "@mui/material";
 // import styles from "./page.module.css";
 import { FormattedMessage } from "react-intl";
 
@@ -7,6 +8,48 @@ export default function Home() {
   return (
     <div>
       <FormattedMessage id="test" description='esto es el testeo'/>
+      <div>
+      <Accordion>
+        <AccordionSummary
+          aria-controls="panel1-content"
+          id="panel1-header"
+        >
+          <Typography component="span">Accordion 1</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          aria-controls="panel2-content"
+          id="panel2-header"
+        >
+          <Typography component="span">Accordion 2</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+      </Accordion>
+      <Accordion defaultExpanded>
+        <AccordionSummary
+          aria-controls="panel3-content"
+          id="panel3-header"
+        >
+          <Typography component="span">Accordion Actions</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          malesuada lacus ex, sit amet blandit leo lobortis eget.
+        </AccordionDetails>
+        <AccordionActions>
+          <Button>Cancel</Button>
+          <Button>Agree</Button>
+        </AccordionActions>
+      </Accordion>
+    </div>
     </div>
   );
 }
