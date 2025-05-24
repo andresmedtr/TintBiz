5e175935494cb10194a1567ddb9fa828943e3040
Rodrigo <rodricelis@hotmail.com>
2025-05-24 18:00:36 -0400

    Adding Intl

---
+++ src/provider/translate/index.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { IntlProvider } from "react-intl";
+
+export const TranslateProvider = ({ children }: { children: React.ReactNode }) => {
+    const [locale, setLocale] = useState("en");
+    const [messages, setMessages] = useState<Record<string, string> | null>(null);
+
+    const loadLocaleData = async (locale: string) => {
+        try {
+            const messages = await import(`../../locales/${locale}.json`);
+            setMessages(messages.default);
+        } catch (error) {
+            console.error("Error loading language:", error);
+        }
+    };
+
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            const browserLocale = navigator.language.startsWith("es") ? "es" : "en";
+            setLocale(browserLocale);
+        }
+    }, []);
+
+    useEffect(() => {
+        loadLocaleData(locale);
+    }, [locale]);
+
+    const changeLocale = (newLocale: string) => {
+        setLocale(newLocale);
+    };
+
+    if (!messages) return null;
+
+    return (
+        <IntlProvider locale={locale} defaultLocale="en" messages={messages}>
+            {children}
+        </IntlProvider>
+    );
+};
