a96e8ce2c8e38254d51ebbae9bfb94cbb92e004c
Rodrigo <rodricelis@hotmail.com>
2025-05-28 23:07:24 -0400

    Firts Block startign styling

--- src/blocks/model3D/index.tsx
+++ src/blocks/model3D/index.tsx
@@ -1,31 +1,36 @@
+"use client";
+
 import { TintAccordion } from "@/components/accordion";
 import ModelViewer from "@/components/modelViewer";
-import Image from "next/image";
-// import { FormattedMessage } from "react-intl";
+import { FormattedMessage } from "react-intl";
 
 export const Model3D = () => {
   return (
-    <div className="w-full h-[fit-content] flex">
-      <div className="">
-        <Image
-          src="/logo/logo1.png"
-          alt="Tints@Home Logo"
-          width={300}
-          height={300}
-        />
-        <div>
-          <TintAccordion />
-        </div>
+    <div className="w-full h-[fit-content]">
+      <div className="w-full h-fit text-center py-[50px]">
+        <h1 className="text-5xl font-bold">
+          <FormattedMessage id="block.model3d.title" />
+        </h1>
+        <p>
+          <FormattedMessage id="block.model3d.parapgrah" />
+        </p>
       </div>
-      <div>
-        <ModelViewer
-          src="https://vq4jjvoakgh0bglr.public.blob.vercel-storage.com/g_wagon-Lng1Z2L9WuWNGZXVDsKZhcHQUlFf0o.glb"
-          alt="Un modelo 3D"
-          auto-rotate={true}
-          camera-controls={true}
-          width="500px"
-          height="500px"
-        />
+      <div className="bg-white w-full h-fit flex justify-center py-[50px]">
+        <div className="h-full flex flex-row border border-zinc-900 items-center">
+          <div>
+            <TintAccordion />
+          </div>
+          <div>
+            <ModelViewer
+              src="https://vq4jjvoakgh0bglr.public.blob.vercel-storage.com/g_wagon-Lng1Z2L9WuWNGZXVDsKZhcHQUlFf0o.glb"
+              alt="Un modelo 3D"
+              auto-rotate={true}
+              camera-controls={true}
+              width="500px"
+              height="500px"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
