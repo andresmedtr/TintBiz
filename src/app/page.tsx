a8051098053e38939e80e47fd82dc1cee2a5b9e9
andresmedtr <123752360+andresmedtr@users.noreply.github.com>
2025-05-29 20:44:54 -0400

    Current changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,11 @@
 import Model3D from "@/blocks/model3D";
 import ContactUs from "@/blocks/contactUs";
 import { TintAccordion } from "@/components/accordion";
-import { SecondaryNavbar } from "@/components/SecondayNavbar";
 
 export default function Home() {
   return (
     <main>
       <Model3D />
-      <SecondaryNavbar />
       <ContactUs />
     </main>
   );
